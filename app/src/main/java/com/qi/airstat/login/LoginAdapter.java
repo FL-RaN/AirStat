e4cace9c1aaf6f19db65fb9b7cb2c3473d70721e
jumpsnack <kir0302@gmail.com>
2016-08-02 14:59:08 -0700

    ForgotPassword 폼 추가 - Not yet

--- app/src/main/java/com/qi/airstat/login/LoginAdapter.java
+++ app/src/main/java/com/qi/airstat/login/LoginAdapter.java
@@ -1,30 +1,84 @@
 package com.qi.airstat.login;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.qi.airstat.R;
+import com.qi.airstat.newAccount.NewAccountActivity;
 
 /**
  * Created by JUMPSNACK on 7/29/2016.
  */
 public class LoginAdapter {
     private View view;
+    private Activity activity;
     private LoginUi loginUi;
+    private LoginCommunication loginCommunication;
 
-    public LoginAdapter(View view){
+    public LoginAdapter(View view, Activity activity) {
         this.view = view;
+        this.activity = activity;
         this.loginUi = new LoginUi();
 
         setWidgets(this.loginUi, this.view);
+        setEvent(this.loginUi);
     }
 
-    private void setWidgets(LoginUi loginUi, View view){
+    private void setWidgets(LoginUi loginUi, View view) {
         loginUi.edtEmail = (EditText) view.findViewById(R.id.edt_login_mail);
         loginUi.edtPassword = (EditText) view.findViewById(R.id.edt_login_pwd);
         loginUi.btnLogin = (Button) view.findViewById(R.id.btn_login_login);
         loginUi.btnForgotPwd = (Button) view.findViewById(R.id.btn_login_forgot_pwd);
         loginUi.btnCreateAccnt = (Button) view.findViewById(R.id.btn_login_create_account);
+    }
+
+    private void setEvent(final LoginUi loginUi) {
+        loginUi.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkFormat(loginUi))
+                    loginCommunication = new LoginCommunication(view.getContext(), loginUi);
+            }
+        });
+
+        loginUi.btnCreateAccnt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.startActivity(new Intent(activity.getApplicationContext(), NewAccountActivity.class));
+            }
+        });
+
+        loginUi.btnForgotPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                activity.startActivity(new Intent(activity.getApplicationContext(), ForgotPasswordActivity.class));
+            }
+        });
+    }
+
+    private boolean checkFormat(LoginUi loginUi) {
+        String email = loginUi.edtEmail.getText().toString().trim();
+        String password = loginUi.edtPassword.getText().toString().trim();
+
+        if (email.getBytes().length <= 0) {
+            makeToast("Please enter an email address");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            makeToast("Please enter a valid email address");
+            return false;
+        } else if (password.getBytes().length <= 0) {
+            makeToast("Please enter a password");
+            return false;
+        }
+        return true;
+    }
+
+    private void makeToast(String msg) {
+        Toast.makeText(view.getContext(), msg, Toast.LENGTH_LONG).show();
     }
 }
