ee5327bd19e4394102a4e93237e6c150aa859bb2
jumpsnack <kir0302@gmail.com>
2016-08-07 01:44:48 -0700

    프로토콜 정의 대기

--- app/src/main/java/com/qi/airstat/HttpService.java
+++ app/src/main/java/com/qi/airstat/HttpService.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.widget.Toast;
 
 import org.json.JSONObject;
@@ -22,6 +23,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by JUMPSNACK on 8/2/2016.
@@ -32,12 +34,15 @@ public class HttpService extends AsyncTask<String, String, String> {
     private ArrayList<String> params;
     ProgressDialog pdLoading;
     HttpURLConnection conn;
+    String strUrl;
     URL url;
 
     DialogFragment dialogFragment;
     FragmentManager fragmentManager;
 
-    public void excuteConn(Context context, ArrayList<String> params, DialogFragment dialogFragment, FragmentManager fragmentManager) {
+    String receivedData;
+
+    public String executeConn(Context context, String strUrl, ArrayList<String> params, DialogFragment dialogFragment, FragmentManager fragmentManager) {
         this.context = context;
         this.params = params;
         this.dialogFragment = dialogFragment;
@@ -45,7 +50,17 @@ public class HttpService extends AsyncTask<String, String, String> {
 
         this.pdLoading = new ProgressDialog(context);
 
-        this.execute();
+        this.strUrl = strUrl;
+        try {
+            receivedData = this.execute(strUrl).get();
+            return receivedData;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
@@ -60,7 +75,7 @@ public class HttpService extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... strings) {
         try {
-            url = new URL(Constants.HTTP_STR_URL);
+            url = new URL(strings[0]);
         } catch (MalformedURLException e) {
             e.printStackTrace();
             return "MalformedURLException";
@@ -89,6 +104,9 @@ public class HttpService extends AsyncTask<String, String, String> {
             conn.setConnectTimeout(Constants.HTTP_CONNECT_TIMEOUT);
             conn.setReadTimeout(Constants.HTTP_READ_TIMEOUT);
             conn.setRequestMethod("POST");
+            conn.setRequestProperty("Cache-Control", "no-cache");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
 
                 /*
                 SetDoInput and setDoOutput method depict handling of both send and receive
@@ -97,22 +115,39 @@ public class HttpService extends AsyncTask<String, String, String> {
             conn.setDoOutput(true);
 
                 /*
-                CASE#1. Query String - Append parameters to URL
-                 */
-//                Uri.Builder builder = new Uri.Builder()
-//                        .appendQueryParameter(Constants.COMMUNICATION_USER_EMAIL, strings[0])
-//                        .appendQueryParameter(Constants.COMMUNICATION_USER_PASSWORD, strings[1]);
-//                String query = builder.build().getEncodedQuery();
-
-
-                /*
-                CASE#2. JSON - Append parameters to JSON
+                JSON - Append parameters to JSON
                  */
             JSONObject jsonQuery = new JSONObject();
             for (int i = 0; i < params.size(); i++) {
                 jsonQuery.put(params.get(i), params.get(++i));
             }
             String query = jsonQuery.toString();
+
+            /*!!!!! JUST FOR TEST !!!!!*/
+            if (strUrl.equals(Constants.HTTP_STR_URL_TEST))
+                query = "{\n" +
+                        "   \"HR\":[{\"timeStamp\":\"190408031123\",\n" +
+                        "    \"connectionID\":1,\n" +
+                        "    \"heartrate\":80,\n" +
+                        "    \"latitude\":33.88,\n" +
+                        "    \"longitude\":-101.11\n" +
+                        "    }],\n" +
+                        "    \"AIR\":[{\n" +
+                        "    \"timeStamp\":\"190805141222\",\n" +
+                        "    \"connectionID\":1,\n" +
+                        "    \"SO2\":23.3,\n" +
+                        "    \"NO2\":13.1,\n" +
+                        "    \"O3\":22.1,\n" +
+                        "    \"CO\":40.1,\n" +
+                        "    \"PM\":7.3,\n" +
+                        "    \"temperature\":38.6,\n" +
+                        "    \"latitude\":33.88,\n" +
+                        "    \"longitude\":-101.11\n" +
+                        "    }]\n" +
+                        "}";
+
+
+            Log.w("SND", query);
 
                 /*
                 Open connection for sending data
@@ -151,8 +186,23 @@ public class HttpService extends AsyncTask<String, String, String> {
                     result.append(line);
                 }
 
-                return result.toString();
+                String strResult = result.toString();
+
+                /* Processing for PROTOCOL */
+//                switch (strResult.charAt(0)) {
+//                    case '1':
+////                        receivedData = strResult;
+//                        return strResult;
+//                    case '2':
+//                        return "false";
+//                    default:
+////                        receivedData = strResult;
+//                        return strResult; //Temporary setting
+//                }
+
+                return strResult;
             } else {
+                Log.w("RSP CODE", String.valueOf(responseCode));
                 return "unsuccessful";
             }
         } catch (IOException e) {
@@ -163,24 +213,49 @@ public class HttpService extends AsyncTask<String, String, String> {
             return "Exception";
         } finally {
             conn.disconnect();
-            return null;
         }
     }
 
     @Override
     protected void onPostExecute(String result) {
         pdLoading.dismiss();
-        if (result.equalsIgnoreCase("true")) {
-            makeToast("Successfully connected!");
-            if (dialogFragment != null)
-                dialogFragment.show(fragmentManager, "");
-        } else if (result.equalsIgnoreCase("false")) {
-            makeToast("Invalid information");
-        } else if (result.equalsIgnoreCase("MalformedURLException") || result.equalsIgnoreCase("Exception") || result.equalsIgnoreCase("IOException") || result.equalsIgnoreCase("unsuccessful")) {
+
+        /* Err CASE*/
+        if (result.equalsIgnoreCase("MalformedURLException") || result.equalsIgnoreCase("Exception") || result.equalsIgnoreCase("IOException") || result.equalsIgnoreCase("unsuccessful")) {
             makeToast("Something went wrong. Connection Problem");
+        } else { /* Success CASE */
+            Log.w("RCV", receivedData);
+
+            if (strUrl.equals(Constants.HTTP_STR_URL_LOGIN)) {
+//                switch (receivedData.charAt(10)){
+//                    case '0':
+//                        makeToast("Verification Failed"); break;
+//                    case '1':
+//                        makeToast("Welcome! verified");
+//                        if (dialogFragment != null)
+//                            dialogFragment.show(fragmentManager, "");
+//                        break;
+//                }
+            } else if (strUrl.equals(Constants.HTTP_STR_URL_CREATE_NEW_ACCOUNT)) {
+//                if (result.equals("")) {
+//                    makeToast("NULL!");
+//                    return;
+//                }
+//
+//                switch (result.charAt(0)) {
+//                    case '0':
+//                        new NewAccountFailureDialog().show(fragmentManager, "");
+//                        break;
+//                    case '1':
+//                        if (dialogFragment != null)
+//                            dialogFragment.show(fragmentManager, "");
+//                        break;
+//                }
+            } else if (strUrl.equals(Constants.HTTP_STR_URL_FORGOT_PASSWORD)) {
+//                if (dialogFragment != null)
+//                    dialogFragment.show(fragmentManager, "");
+            }
         }
-//        if (dialogFragment != null)
-//            dialogFragment.show(fragmentManager, "");
     }
 
     private void makeToast(String msg) {
