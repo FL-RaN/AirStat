780970f076a871ef9c60fde395d70b16e2d83c3e
jumpsnack <kir0302@gmail.com>
2016-08-07 01:52:58 -0700

    구글맵 슬라이더 패널 유저인터페이스 클래스

---
+++ app/src/main/java/com/qi/airstat/dataMap/DataMapUi.java
@@ -0,0 +1,17 @@
+package com.qi.airstat.dataMap;
+
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+/**
+ * Created by JUMPSNACK on 8/3/2016.
+ */
+public class DataMapUi {
+    public SlidingUpPanelLayout slidingUpPanelLayout;
+    public LinearLayout barTitle;
+    public TextView tvTitle;
+    public TextView tvAqiGrade;
+    public TextView tvTemp;
+}
