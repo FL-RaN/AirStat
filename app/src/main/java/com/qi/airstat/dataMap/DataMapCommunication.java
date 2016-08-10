6aeaab7b5b17c56956d71b75b4c9a9a3b52ce053
jumpsnack <kir0302@gmail.com>
2016-08-10 00:56:42 -0700

    AsyncTask -> Thread 전환

--- app/src/main/java/com/qi/airstat/dataMap/DataMapCommunication.java
+++ app/src/main/java/com/qi/airstat/dataMap/DataMapCommunication.java
@@ -1,126 +1,260 @@
 package com.qi.airstat.dataMap;
 
 import android.content.Context;
-import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.qi.airstat.Constants;
-import com.qi.airstat.HttpService;
 import com.qi.airstat.iHttpConnection;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
- * Created by JUMPSNACK on 8/5/2016.
+ * Created by JUMPSNACK on 8/9/2016.
  */
-public class DataMapCommunication extends AsyncTask<Void, Void, Void> implements iHttpConnection {
-    private HttpService httpService;
-    private boolean flow = true;
-    private String result = null;
+public class DataMapCommunication extends Thread implements iHttpConnection {
+
+    private HttpThread httpThread;
+    private boolean flow = false;
+    private String result = "";
     private DataMapActivity dataMapActivity;
 
     private Context context;
-    ArrayList<String> params;
+    private ArrayList<String> params;
 
+    private DataMapCommunication() {
+    }
 
     public DataMapCommunication(Context context, DataMapActivity dataMapActivity) {
         this.context = context;
         this.dataMapActivity = dataMapActivity;
     }
 
-    public void stop() {
-        this.flow = false;
-        this.cancel(true);
+    public void wake() {
+        synchronized (this) {
+
+            if (this.flow && getState() == State.TIMED_WAITING) {
+                try {
+//                    Log.w("STATUS", this.getState() + "");
+                    this.interrupt();
+                } catch (Exception e) {
+//                    Log.w("WAKE", "FAIL");
+                }
+            }
+        }
     }
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
+    public void stopThread() {
+        this.flow = false;
+    }
 
+    private ArrayList onPreExecute() {
         params = new ArrayList<>();
         params.add(Constants.HTTP_MSG_ID);
         params.add(Constants.HTTP_REQUEST_ONGOING_SESSION + "");
         params.add(Constants.HTTP_DATA_MAP_RANGE_MIN_LAT);
-        params.add(DataMapCurrentLocation.getInstance().getMinLat() + "");
+        params.add(DataMapCurrentUser.getInstance().getMinLat() + "");
         params.add(Constants.HTTP_DATA_MAP_RANGE_MAX_LAT);
-        params.add(DataMapCurrentLocation.getInstance().getMaxLat() + "");
+        params.add(DataMapCurrentUser.getInstance().getMaxLat() + "");
         params.add(Constants.HTTP_DATA_MAP_RANGE_MIN_LNG);
-        params.add(DataMapCurrentLocation.getInstance().getMinLng() + "");
+        params.add(DataMapCurrentUser.getInstance().getMinLng() + "");
         params.add(Constants.HTTP_DATA_MAP_RANGE_MAX_LNG);
-        params.add(DataMapCurrentLocation.getInstance().getMaxLng() + "");
+        params.add(DataMapCurrentUser.getInstance().getMaxLng() + "");
 
+        return params;
     }
 
     @Override
-    protected Void doInBackground(Void... voids) {
+    public void run() {
+        super.run();
+
+        flow = true;
         while (flow) {
-
-            result = executeHttpConn();
-            resultHandler(result);
-
+//            onPreExecute();
             try {
-                Thread.sleep(Constants.HTTP_DATA_MAP_ONGOING_TIME_QUALTUM);
-            } catch (InterruptedException e) {
+                resultHandler(executeHttpConn());
+                Thread.sleep(Constants.HTTP_DATA_MAP_GET_ONGOING_SESSION_TIME_QUALTUM);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    }
 
-        return null;
+    public void resultHandler(String result) {
+        Log.w("result", result + "");
+        JSONObject rcvdData = null;
+
+        try {
+            rcvdData = new JSONObject(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            onPostExecute(result);
+            return;
+        }
+        if (onPostExecute(result)) {
+            parseOngoingSessionData(rcvdData);
+        }
     }
 
     @Override
     public String executeHttpConn() {
-        httpService = new HttpService();
-        return httpService.executeConn(context, Constants.HTTP_STR_URL_ONGOING_SESSION, params);
-    }
-
-    public void resultHandler(String result) {
-        JSONObject rcvdData = null;
-        int responseCode = -1;
-
-        try {
-            rcvdData = new JSONObject(result);
-            responseCode = rcvdData.getInt(Constants.HTTP_RESPONSE_RESULT);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        switch (responseCode) {
-            case Constants.HTTP_RESPONSE_RESULT_OK:
-                parseOngoingSessionData(rcvdData);
-                break;
-        }
+        httpThread = new HttpThread();
+        return httpThread.executeConn(DataMapActivity.context, Constants.HTTP_STR_URL_ONGOING_SESSION, params);
     }
 
     public void parseOngoingSessionData(JSONObject rcvdData) {
         try {
-            int count = rcvdData.getInt(Constants.HTTP_DATA_MAP_ONGOING_SESSION_COUNT);
-            JSONArray data = rcvdData.getJSONArray(Constants.HTTP_DATA_MAP_ONGOING_SESSION_DATA);
-
             dataMapActivity.initMarkerCollection();
 
-            for (int i = 0; i < count; i++) {
-                JSONObject eachData = (JSONObject) data.get(i);
-                double temperature = eachData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_TEMP);
-                double co = eachData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_CO);
-                double so2 = eachData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_SO2);
-                double no2 = eachData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_NO2);
-                double o3 = eachData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_O3);
-                double pm = eachData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_PM);
-                double lat = eachData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_LAT);
-                double lng = eachData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_LNG);
+            int i = 1;
+            JSONObject eachData = rcvdData.getJSONObject("" + (i++));
 
-                dataMapActivity.addMarker("", new DataMapDataSet(temperature, co, so2, no2, o3, pm), new LatLng(lat, lng));
+            while (eachData != null) {
+
+                int connectionID = eachData.getInt(Constants.HTTP_DATA_MAP_ONGOING_SESSION_CID);
+                long timeStamp = eachData.getLong(Constants.HTTP_DATA_MAP_ONGOING_SESSION_TIME_STAMP);
+                JSONObject airData = eachData.getJSONObject(Constants.HTTP_DATA_MAP_ONGOING_SESSION_AIR);
+
+                double temperature = airData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_TEMP);
+                double co = airData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_CO);
+                double so2 = airData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_SO2);
+                double no2 = airData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_NO2);
+                double o3 = airData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_O3);
+                double pm = airData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_PM);
+                double lat = airData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_LAT);
+                double lng = airData.getDouble(Constants.HTTP_DATA_MAP_ONGOING_SESSION_LNG);
+
+                dataMapActivity.addMarker(connectionID, timeStamp, new DataMapDataSet(temperature, co, so2, no2, o3, pm), new LatLng(lat, lng));
+                try {
+                    eachData = rcvdData.getJSONObject("" + (i++));
+                } catch (JSONException e) {
+                    eachData = null;
+                }
             }
-
-//            dataMapActivity.removeAndDrawMarker();
+            dataMapActivity.addMarker(DataMapCurrentUser.create());
+            dataMapActivity.refreshMap();
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    protected boolean onPostExecute(String result) {
+        /* Err CASE*/
+        if (result.equalsIgnoreCase("MalformedURLException") || result.equalsIgnoreCase("Exception") || result.equalsIgnoreCase("IOException") || result.equalsIgnoreCase("unsuccessful")) {
+            makeToast("Something went wrong. Connection Problem");
+            return false;
+        } else { /* Success CASE */
+            return true;
+        }
+    }
+
+
+    private class HttpThread {
+
+        private Context context;
+        private ArrayList<String> params;
+        HttpURLConnection conn;
+        String strUrl;
+        URL url;
+        String rcvdData;
+
+        public String executeConn(Context context, String strUrl, ArrayList<String> params) {
+            this.context = context;
+            this.params = params;
+            this.strUrl = strUrl;
+
+            rcvdData = this.connection(strUrl);
+            return rcvdData;
+        }
+
+        private String connection(String strUrl) {
+            try {
+                url = new URL(strUrl);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            String sendingResult = sendToServer();
+            if (sendingResult != null) return sendingResult;
+
+            String receivingResult = receiveFromServer();
+            if (receivingResult != null) return receivingResult;
+
+            return null;
+        }
+
+
+        private String sendToServer() {
+            try {
+                /*
+                Set HttpURLConnection to send and receive data from php and mysql
+                 */
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setConnectTimeout(Constants.HTTP_CONNECT_TIMEOUT);
+                conn.setReadTimeout(Constants.HTTP_READ_TIMEOUT);
+                conn.setRequestMethod("GET");
+
+                Log.w("URL", strUrl);
+
+                conn.connect();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "SND IOException";
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "SND Exception";
+            }
+            return null;
+        }
+
+
+        private String receiveFromServer() {
+            try {
+                int responseCode = conn.getResponseCode();
+                Log.w("RSP CODE", String.valueOf(responseCode));
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    String strResult = result.toString();
+
+                    return strResult;
+                } else {
+                    return "unsuccessful";
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "RCV IOException";
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "RCV Exception";
+            } finally {
+                conn.disconnect();
+            }
+        }
+    }
+
+    private void makeToast(String msg) {
+        if (context != null)
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
     }
 }
